45c9d9d48f60744b12e3f054c476d22bf6142317
amelie certin <amelie.certin@gmail.com>
2017-04-17 15:57:41 +0200

    Change migration to model & add seed

---
+++ seeders/20170417132308-add-users-seeder.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('users', [{
+        email: 'alex@email.fr',
+        password: 'password',
+        firstname: 'Alexandre',
+        lastname: 'Lairan',
+        nickname: 'Necros',
+        isAdmin: true,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }, {
+        email: 'amelie@email.fr',
+        password: 'password',
+        firstname: 'Amelie',
+        lastname: 'Certin',
+        nickname: 'Kuma',
+        isActiv: false,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }, {
+        email: 'yohan@email.fr',
+        password: 'password',
+        firstname: 'Yohan',
+        lastname: 'Fairfort',
+        nickname: 'Yo',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ]);
+  },
+
+  down: function (queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('users', null, {});
+  }
+};
