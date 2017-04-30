0f7dc93e1095b6c825df01a6030a6d0087ba68fe
amelie certin <amelie.certin@gmail.com>
2017-04-30 17:55:06 +0200

    Add task resource

---
+++ models/tasks.js
@@ -0,0 +1,32 @@
+'use strict'
+//sequelize model:create --name Users --attributes first_name:string,last_name:string
+
+module.exports = function(sequelize, DataTypes) {
+    var Tasks = sequelize.define('tasks', {
+        id: {
+            type: DataTypes.BIGINT,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            defaultValue: ''
+        },
+        description: {
+            type: DataTypes.TEXT,
+            defaultValue: ''
+        },
+        type: {
+            type: DataTypes.ENUM('Theme', 'User story', 'Task'),
+        }
+    }, {
+        classMethods: {
+            associate: function(models) {
+                Tasks.hasMany(Tasks, {
+                    as: 'ancestor'
+                })
+            }
+        }
+    });
+    return Tasks;
+}
