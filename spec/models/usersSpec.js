1a4203846ff6a39a38ea58789fcb590126f50bed
Fairfort.yohan <yohan.fairfort@gmail.com>
2017-05-06 16:25:34 +0200

    tryout Jasmine, need async test

--- spec/models/usersSpec.js
+++ spec/models/usersSpec.js
@@ -1,11 +1,34 @@
 "use strict"
+let models = require('../../models');
+let users = models.users;
+
+describe("users model",function(){
+  it("create a null model", function(){
+    users.create({
+      email: "email",
+      password: "password"
+    }).then(function(user) {
+      expect(user).not.toEqual({
+        email:"email",
+        password:"password"
+      });
+
+      console.log(user);
+    })
+  });
+});
+
 function HelloWorld()
 {
-  return "Hello World";
+  return "HelloWorld";
 }
 
-describe("Hello World",function(){
-  it("says Hello", function(){
-    expect(HelloWorld()).toEqual("Hello World");
+describe("HelloWorld",function(){
+  it("hey hello success", function(){
+    expect(HelloWorld()).toEqual("HelloWorld");
+  });
+
+  it("hey hello not success", function(){
+    expect(HelloWorld()).not.toEqual("HelloWorld");
   });
 });
