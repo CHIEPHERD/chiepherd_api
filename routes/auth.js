8def44a54bcd60348b36dc75f4359141d8798a9b
amelie certin <amelie.certin@gmail.com>
2017-05-14 00:09:45 +0200

    Add register & amq msg after create

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var passport = require('passport')
+const models = require('../models');
+const User = models.users;
 var router = express.Router();
 
 router.post('/login', passport.authenticate('local'), function(req, res) {
-  res.json({
-    result: 'ok'
-  })
+  res.redirect('/');
 });
 
 router.get('/logout', function(req, res){
@@ -15,7 +15,24 @@ router.get('/logout', function(req, res){
 router.get('/', function(req, res) {
   res.json({
     result: 'ok'
-  })
-})
+  });
+});
+
+router.post('/register', function(req, res) {
+  if (req.body.password == req.body.password_confirmation) {
+    User.create({
+      email: req.body.email,
+      password: req.body.password
+    }).then(function() {
+      passport.authenticate('local') (req, res, function () {
+        res.redirect('/');
+      });
+    }).catch(function(error) {
+      res.status(422).send(error);
+    });
+  } else {
+    res.status(422).send('Password and password confirmation are different');
+  }
+});
 
 module.exports = router;
