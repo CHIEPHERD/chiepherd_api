3d0b4ed8a0ad72d5d507a4caa0556b33f35724ca
amelie certin <amelie.certin@gmail.com>
2017-06-23 15:34:39 +0200

    Search task by uuid

--- jobs/tasks/create.js
+++ jobs/tasks/create.js
@@ -16,29 +16,44 @@ module.exports = function(connection, done) {
         let json = JSON.parse(msg.content.toString());
 
         // Create task
-        Task.create({
-          uuid: uuidV4(),
-          title: json.title,
-          description: json.description,
-          type: json.label,
-          projectId: json.projectId,
-          ancestorId: json.ancestorId
-        }).then(function(task) {
-          ch.sendToQueue(msg.properties.replyTo,
-            new Buffer.from(JSON.stringify(task)),
-            { correlationId: msg.properties.correlationId });
-          connection.createChannel(function(error, channel) {
-            var ex = 'chiepherd.task.created';
-            channel.assertExchange(ex, 'fanout', { durable: false });
-            channel.publish(ex, '', new Buffer.from(JSON.stringify(task)));
-          });
-          ch.ack(msg);
-        }).catch(function(error) {
-          ch.sendToQueue(msg.properties.replyTo,
-            new Buffer(error.toString()),
-            { correlationId: msg.properties.correlationId });
-          ch.ack(msg);
-        });
+        Project.find({
+          where: {
+            uuid: json.projectUuid
+          }
+        }).then(function (project) {
+          Task.find({
+            where: {
+              uuid: json.ancestorUuid
+            }
+          }).then(function (ancestor) {
+            projectId = (project != null ? project.id : null)
+            ancestorId = (ancestor != null ? ancestor.id : null)
+            Task.create({
+              uuid: uuidV4(),
+              title: json.title,
+              description: json.description,
+              type: json.label,
+              projectId: projectId,
+              ancestorId: ancestorId
+            }).then(function(task) {
+              ch.sendToQueue(msg.properties.replyTo,
+                new Buffer.from(JSON.stringify(task)),
+                { correlationId: msg.properties.correlationId });
+              connection.createChannel(function(error, channel) {
+                var ex = 'chiepherd.task.created';
+                channel.assertExchange(ex, 'fanout', { durable: false });
+                channel.publish(ex, '', new Buffer.from(JSON.stringify(task)));
+              });
+              ch.ack(msg);
+            }).catch(function(error) {
+              ch.sendToQueue(msg.properties.replyTo,
+                new Buffer(error.toString()),
+                { correlationId: msg.properties.correlationId });
+              ch.ack(msg);
+            });
+          })
+        })
+
       }, { noAck: false });
     });
   });
