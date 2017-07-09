3e937f151729756c8c10348a7a11dac4ddd8ae92
amelie certin <amelie.certin@gmail.com>
2017-07-09 14:58:44 +0200

    Fix task.update

--- jobs/tasks/update.js
+++ jobs/tasks/update.js
@@ -20,35 +20,55 @@ module.exports = function(connection, done) {
             uuid: json.uuid
           }
         }).then(function(task) {
-          Task.find({
-            where: {
-              uuid: json.ancestorUuid
-            }
-          }).then(function (ancestor) {
-            if(task) {
-              task.update({
-                title: json.name || task.title,
-                description: json.description || task.description,
-                type: json.type || task.type,
-                ancestorId: (ancestor && ancestor.id) || task.ancestorId
-              }).then(function(task) {
-                ch.sendToQueue(msg.properties.replyTo,
-                  new Buffer.from(JSON.stringify(task.responsify())),
-                  { correlationId: msg.properties.correlationId });
-                connection.createChannel(function(error, channel) {
-                  var ex = 'chiepherd.task.updated';
-                  channel.assertExchange(ex, 'fanout', { durable: false });
-                  channel.publish(ex, '', new Buffer.from(JSON.stringify(task)));
+          if(task != undefined) {
+            Task.find({
+              where: {
+                uuid: json.ancestorUuid
+              }
+            }).then(function (ancestor) {
+              console.log(ancestor.responsify());
+              console.log(json.ancestorUuid);
+              if (ancestor != undefined || json.ancestorUuid == undefined || json.ancestorUuid == null) {
+                task.update({
+                  title: json.name || task.title,
+                  description: json.description || task.description,
+                  type: json.type || task.type,
+                  ancestorId: (ancestor && ancestor.id) || task.ancestorId
+                }).then(function(task) {
+                  task.ancestor = ancestor;
+                  ch.sendToQueue(msg.properties.replyTo,
+                    new Buffer.from(JSON.stringify(task.responsify())),
+                    { correlationId: msg.properties.correlationId });
+                  connection.createChannel(function(error, channel) {
+                    var ex = 'chiepherd.task.updated';
+                    channel.assertExchange(ex, 'fanout', { durable: false });
+                    channel.publish(ex, '', new Buffer.from(JSON.stringify(task.responsify())));
+                  });
+                  ch.ack(msg);
+                }).catch(function(error) {
+                  ch.sendToQueue(msg.properties.replyTo,
+                    new Buffer(error.toString()),
+                    { correlationId: msg.properties.correlationId });
+                  ch.ack(msg);
                 });
-                ch.ack(msg);
-              }).catch(function(error) {
+              } else {
                 ch.sendToQueue(msg.properties.replyTo,
-                  new Buffer(error.toString()),
+                  new Buffer("Unknown ancestor task."),
                   { correlationId: msg.properties.correlationId });
                 ch.ack(msg);
-              });
-            }
-          })
+              }
+            }).catch(function (error) {
+              ch.sendToQueue(msg.properties.replyTo,
+                new Buffer(error.toString()),
+                { correlationId: msg.properties.correlationId });
+              ch.ack(msg);
+            });
+          } else {
+            ch.sendToQueue(msg.properties.replyTo,
+              new Buffer("Unknown task."),
+              { correlationId: msg.properties.correlationId });
+            ch.ack(msg);
+          }
         }).catch(function(error) {
           ch.sendToQueue(msg.properties.replyTo,
             new Buffer(error.toString()),
