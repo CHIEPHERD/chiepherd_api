7c36b4e383556a171797b3ee0143db05f2a3cb70
yohan fairfort <yohan.fairfort@netapsys.fr>
2017-07-18 15:30:00 +0200

    Added Cross origin with Locahost

--- app.js
+++ app.js
@@ -25,6 +25,29 @@ const User = models.users
 //   force: true
 // })
 
+// Add headers
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  if (req.method === "OPTIONS")
+    res.sendStatus(200);
+  else
+    next();
+});
+
 // Configure the local strategy for use by Passport.
 //
 // The local strategy require a `verify` function which receives the credentials
@@ -96,6 +119,8 @@ app.use('/admin', admin);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
+  // Website you wish to allow to connect
+
   err.status = 404;
   next(err);
 });
